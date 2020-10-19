da1fc333ce92d5b7fe021829c778f3b5e670db7d
Trần Tiến <tientm@nal.vn>
2020-10-19 09:07:22 +0700

    layout component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,17 +1,20 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import styles from './styles.module.scss';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
     return (
-        <div>
+        <Layout>
             <Head>
                 <title>First post</title>
             </Head>
+            <p className={styles.weight}>Strong font</p>
             <h2>
                 <Link href="/">
                     <a>Home page</a>
                 </Link>
             </h2>
-        </div>
+        </Layout>
     )
 }
